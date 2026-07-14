07e06f00cc0103f888b1aa33dc3c19bb653b092d
snehalgavhane33 <snehalgavhane33@gmail.com>
2026-07-14 23:50:13 +0530

    updated file

--- Darray.java
+++ Darray.java
@@ -284,57 +284,250 @@ import java.util.*;
 // }
 
 
+// public class Darray {
+
+//     public static void zeroshift(int arr[], int n) {
+
+//         int j = -1;
+
+        
+//         for (int i = 0; i < n; i++) {
+//             if (arr[i] == 0) {
+//                 j = i;
+//                 break;
+//             }
+//         }
+
+        
+//         if (j == -1) {
+//             return;
+//         }
+
+        
+//         for (int i = j + 1; i < n; i++) {
+//             if (arr[i] != 0) {
+
+//                 int temp = arr[i];
+//                 arr[i] = arr[j];
+//                 arr[j] = temp;
+
+//                 j++;
+//             }
+//         }
+//     }
+
+//     public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+
+//     System.out.print("Enter size of array: ");
+//     int n = sc.nextInt();
+//     int[] arr = new int[n];
+
+//     System.out.println("Enter array elements:");
+//     for (int i = 0; i < n; i++) {
+//         arr[i] = sc.nextInt();
+//     }
+
+//     zeroshift(arr, n);
+
+//     System.out.println("Array after shifting zeros to end:");
+//     for (int x : arr) {
+//         System.out.print(x + " ");
+//     }
+//     System.out.println();
+//     sc.close();
+// }
+// }
+
+// public class Darray{
+//     public static int linearsearch(int arr[] , int n , int num){
+//         for(int i=0; i<n; i++){
+//             if(arr[i]==num){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+//     public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+
+//     System.out.print("Enter size of array: ");
+//     int n = sc.nextInt();
+//     int[] arr = new int[n];
+
+//     System.out.println("Enter array elements:");
+//     for (int i = 0; i < n; i++) {
+//         arr[i] = sc.nextInt();
+//     }
+
+//     System.out.print("Enter the number to search: ");
+//     int num = sc.nextInt();
+
+//     int result = linearsearch(arr, n, num);
+
+//     if (result != -1) {
+//         System.out.println("Element found at index: " + result);
+//     } else {
+//         System.out.println("Element not found");
+//     }
+//     sc.close();
+// }
+// }
+
+
+
+
+// public class Darray {
+
+//     public static ArrayList<Integer> unionArray(int[] nums1, int[] nums2) {
+//         ArrayList<Integer> union = new ArrayList<>();
+
+//         int i = 0, j = 0;
+//         int n = nums1.length;
+//         int m = nums2.length;
+
+//         while (i < n && j < m) {
+
+//             if (nums1[i] == nums2[j]) {
+//                 if (union.size() == 0 || union.get(union.size() - 1) != nums1[i]) {
+//                     union.add(nums1[i]);
+//                 }
+//                 i++;
+//                 j++;
+//             }
+
+//             else if (nums1[i] < nums2[j]) {
+//                 if (union.size() == 0 || union.get(union.size() - 1) != nums1[i]) {
+//                     union.add(nums1[i]);
+//                 }
+//                 i++;
+//             }
+
+//             else {
+//                 if (union.size() == 0 || union.get(union.size() - 1) != nums2[j]) {
+//                     union.add(nums2[j]);
+//                 }
+//                 j++;
+//             }
+//         }
+
+//         while (i < n) {
+//             if (union.size() == 0 || union.get(union.size() - 1) != nums1[i]) {
+//                 union.add(nums1[i]);
+//             }
+//             i++;
+//         }
+
+//         while (j < m) {
+//             if (union.size() == 0 || union.get(union.size() - 1) != nums2[j]) {
+//                 union.add(nums2[j]);
+//             }
+//             j++;
+//         }
+
+//         return union;
+//     }
+
+//      public static void main(String[] args) {
+
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter size of first array: ");
+//         int n1 = sc.nextInt();
+
+//         int[] a = new int[n1];
+
+//         System.out.println("Enter first sorted array:");
+//         for (int i = 0; i < n1; i++) {
+//             a[i] = sc.nextInt();
+//         }
+
+//         System.out.print("Enter size of second array: ");
+//         int n2 = sc.nextInt();
+
+//         int[] b = new int[n2];
+
+//         System.out.println("Enter second sorted array:");
+//         for (int i = 0; i < n2; i++) {
+//             b[i] = sc.nextInt();
+//         }
+
+//         ArrayList<Integer> ans = unionArray(a, b);
+
+//         System.out.println("Union of two arrays:");
+//         for (int x : ans) {
+//             System.out.print(x + " ");
+//         }
+
+//         sc.close();
+//     }
+// }
+
+
+
+
 public class Darray {
 
-    public static void zeroshift(int arr[], int n) {
+    public static ArrayList<Integer> intersectionArray(int[] nums1, int[] nums2) {
+        ArrayList<Integer> intersection = new ArrayList<>();
 
-        int j = -1;
+        int i = 0, j = 0;
 
-        
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == 0) {
-                j = i;
-                break;
+        while (i < nums1.length && j < nums2.length) {
+
+            if (nums1[i] == nums2[j]) {
+
+                if (intersection.size() == 0 ||
+                    intersection.get(intersection.size() - 1) != nums1[i]) {
+                    intersection.add(nums1[i]);
+                }
+
+                i++;
+                j++;
             }
-        }
 
-        
-        if (j == -1) {
-            return;
-        }
+            else if (nums1[i] < nums2[j]) {
+                i++;
+            }
 
-        
-        for (int i = j + 1; i < n; i++) {
-            if (arr[i] != 0) {
-
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-
+            else {
                 j++;
             }
         }
+
+        return intersection;
     }
 
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-    System.out.print("Enter size of array: ");
-    int n = sc.nextInt();
-    int[] arr = new int[n];
+        // First array
+        System.out.print("Enter size of first array: ");
+        int n = sc.nextInt();
 
-    System.out.println("Enter array elements:");
-    for (int i = 0; i < n; i++) {
-        arr[i] = sc.nextInt();
+        int[] nums1 = new int[n];
+
+        System.out.println("Enter elements of first sorted array:");
+        for (int i = 0; i < n; i++) {
+            nums1[i] = sc.nextInt();
+        }
+
+        // Second array
+        System.out.print("Enter size of second array: ");
+        int m = sc.nextInt();
+
+        int[] nums2 = new int[m];
+
+        System.out.println("Enter elements of second sorted array:");
+        for (int i = 0; i < m; i++) {
+            nums2[i] = sc.nextInt();
+        }
+
+        ArrayList<Integer> result = intersectionArray(nums1, nums2);
+
+        System.out.println("Intersection of arrays:");
+        System.out.println(result);
+
+        sc.close();
     }
-
-    zeroshift(arr, n);
-
-    System.out.println("Array after shifting zeros to end:");
-    for (int x : arr) {
-        System.out.print(x + " ");
-    }
-    System.out.println();
-    sc.close();
-}
 }
