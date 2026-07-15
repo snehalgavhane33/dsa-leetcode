3272e9245bf467ed9868cfd57b664aaaad4c66e8
snehalgavhane33 <snehalgavhane33@gmail.com>
2026-07-15 11:48:38 +0530

    updated status

--- Darray.java
+++ Darray.java
@@ -532,3 +532,102 @@ import java.util.*;
 //     }
 // }
 
+
+//find missing number optimal 1
+// public class Darray{
+//     public static int missingValue(int[] arr, int n ){
+//         int sum = (n*(n+1))/2;
+//         int s2=0;
+//         for(int i=0; i<n-1; i++){
+//             s2 += arr[i];
+//         }
+//         return sum - s2;
+
+//     }
+
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         // do not use for Q10--------------------------------------------------------------------------------
+//         System.out.print("Enter the size of the array: ");
+//         int n = sc.nextInt();
+
+//         int arr[] = new int[n];
+        
+//         System.out.println("Enter the elements of the array:");
+//         for(int i =0; i<n; i++){
+//             arr[i] = sc.nextInt();
+//         }
+
+//          //11.Call to Find missing number(Optimal 1)
+//         System.out.println("The missing value is: " + missingValue(arr, n));
+        
+//     }
+// }
+
+//find missing value optimal 2
+// public class Darray{
+//      public static int missingValue2(int [] arr, int n){
+//         int xor1=0;
+//         int xor2=0;
+//         for(int i =0; i<n-1; i++){
+//             xor2 = xor2 ^ arr[i];
+//             xor1 = xor1 ^ (i+1);
+//         }
+//         xor1 = xor1 ^ n;
+//         return xor1 ^ xor2;
+//     }
+
+//      public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         // do not use for Q10--------------------------------------------------------------------------------
+//         System.out.print("Enter the size of the array: ");
+//         int n = sc.nextInt();
+
+//         int arr[] = new int[n];
+        
+//         System.out.println("Enter the elements of the array:");
+//         for(int i =0; i<n; i++){
+//             arr[i] = sc.nextInt();
+//         }
+//          System.out.println("The missing value is: " + missingValue2(arr, n));
+
+// }
+// }
+
+//consecutive one find problem
+public class Darray {
+    public static int consecutiveOne(int[] arr, int n){
+        int maxi = 0;
+        int cnt = 0;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] == 1){
+                cnt++;
+                maxi=Math.max(maxi,cnt);
+            }
+            else{
+                cnt = 0;
+            }
+        }
+        return maxi;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter array elements (0 or 1):");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int result = consecutiveOne(nums, n);
+
+        System.out.println("Maximum consecutive ones = " + result);
+
+        sc.close();
+    }
+    
+}
