dc370db27775ad6cb7182f7011ff235c00ec71f8
snehalgavhane33 <snehalgavhane33@gmail.com>
2026-07-17 11:56:26 +0530

    updated file

--- Darray.java
+++ Darray.java
@@ -595,39 +595,169 @@ import java.util.*;
 // }
 
 //consecutive one find problem
-public class Darray {
-    public static int consecutiveOne(int[] arr, int n){
-        int maxi = 0;
-        int cnt = 0;
-        for(int i=0; i<arr.length; i++){
-            if(arr[i] == 1){
-                cnt++;
-                maxi=Math.max(maxi,cnt);
+// public class Darray {
+//     public static int consecutiveOne(int[] arr, int n){
+//         int maxi = 0;
+//         int cnt = 0;
+//         for(int i=0; i<arr.length; i++){
+//             if(arr[i] == 1){
+//                 cnt++;
+//                 maxi=Math.max(maxi,cnt);
+//             }
+//             else{
+//                 cnt = 0;
+//             }
+//         }
+//         return maxi;
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter size of array: ");
+//         int n = sc.nextInt();
+
+//         int[] nums = new int[n];
+
+//         System.out.println("Enter array elements (0 or 1):");
+//         for (int i = 0; i < n; i++) {
+//             nums[i] = sc.nextInt();
+//         }
+
+//         int result = consecutiveOne(nums, n);
+
+//         System.out.println("Maximum consecutive ones = " + result);
+
+//         sc.close();
+//     }
+    
+// }
+
+// find once appering no
+
+// public class Darray{
+//     public static int singleNumbaer(int[] arr, int n){
+//         int xor = 0;
+//         for(int i=0; i<n; i++){
+//             xor = xor ^ arr[i];
+//         }
+//         return xor;
+//     }
+    
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter size of array: ");
+//         int n = sc.nextInt();
+
+//         int[] arr = new int[n];
+//         System.out.println("Enter the elements:");
+//         for (int i = 0; i < n; i++) {
+//             arr[i] = sc.nextInt();
+//         }
+
+//         int single = singleNumbaer(arr, n);
+
+//         System.out.println("Single number is: " + single);
+//     }
+// }
+
+// public class Darray{
+//     public static int longestSubarray(int[] arr, int k){
+//         HashMap<Long, Integer> preSumMap = new HashMap<>();
+//         long sum = 0;
+//         int maxLen = 0;
+//         for(int i=0; i<arr.length; i++){
+//             sum += arr[i];
+
+//             if(sum == k){
+//                 maxLen = Math.max(maxLen , i+1);
+//             }
+
+//             long rem = sum - k;
+
+//             if (preSumMap.containsKey(rem)){
+//                 int len = i - preSumMap.get(rem);
+//                 maxLen = Math.max(maxLen,len);
+//             }
+
+//             if (!preSumMap.containsKey(sum)) {
+//                 preSumMap.put(sum, i);
+//             }
+//         }
+//         return maxLen;
+
+//     }
+//      public static void main(String[] args) {
+
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter size of array: ");
+//         int n = sc.nextInt();
+
+//         int[] arr = new int[n];
+
+//         System.out.println("Enter array elements:");
+//         for (int i = 0; i < n; i++) {
+//             arr[i] = sc.nextInt();
+//         }
+
+//         System.out.print("Enter K: ");
+//         int k = sc.nextInt();
+
+//         int ans = longestSubarray(arr, k);
+
+//         System.out.println("Longest Subarray Length = " + ans);
+
+//         sc.close();
+//     }
+// }
+
+public class Darray{
+    public static int longestSubarray(int[] arr, int k){
+        int left =0;
+        int right =0;
+
+        long sum = arr[0];
+        int maxLen = 0;
+        int n = arr.length;
+
+        while (right < n){
+            while(left <= right && sum > k){
+                sum -= arr[left];
+                left ++;
             }
-            else{
-                cnt = 0;
+
+            if(sum == k){
+                maxLen = Math.max(maxLen, right-left+1);
+;
+            }
+            right++;
+
+            if(right < n){
+                sum += arr[right]++;
             }
         }
-        return maxi;
+        return maxLen;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter size of array: ");
+        System.out.println("Enter size of array");
         int n = sc.nextInt();
+        int[] arr = new int[n];
 
-        int[] nums = new int[n];
-
-        System.out.println("Enter array elements (0 or 1):");
-        for (int i = 0; i < n; i++) {
-            nums[i] = sc.nextInt();
+        System.out.println("Enter array element:");
+        for(int i=0; i<n; i++){
+            arr[i]= sc.nextInt();
         }
 
-        int result = consecutiveOne(nums, n);
+        System.out.println("Enter k:");
+        int k = sc.nextInt();
 
-        System.out.println("Maximum consecutive ones = " + result);
+        int ans = longestSubarray(arr, k);
+
+        System.out.println("Longest Length = " + ans);
 
         sc.close();
     }
-    
 }
