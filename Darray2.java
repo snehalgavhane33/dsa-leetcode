acefc116a3c0737fa4830bd470ca00f1e87c6a75
snehalgavhane33 <snehalgavhane33@gmail.com>
2026-07-28 14:14:47 +0530

    update Darray2.java

--- Darray2.java
+++ Darray2.java
@@ -752,33 +752,104 @@ import java.util.*;
 // }
 
 
-//set matrix zeros (btter)
-public class Darray2{
-    public static int[][] matrixZeros(int arr[][], int n, int m){
-        int[] row = new int[n];
-        int[] col = new int[m];
+//set matrix zeros (better)
+// public class Darray2{
+//     public static int[][] matrixZeros(int arr[][], int n, int m){
+//         int[] row = new int[n];
+//         int[] col = new int[m];
 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
+//         for(int i=0; i<n; i++){
+//             for(int j=0; j<m; j++){
+//                 if(arr[i][j] == 0){
+//                     row[i]=1;
+//                     col[j]=1;
+
+//                 }
+//             }
+//         }
+
+//         for(int i=0; i<n; i++){
+//             for(int j=0; j<m; j++){
+//                 if(row[i]==1 || col[j]==1){
+//                     arr[i][j]=0;
+//                 }
+//             }
+//         }
+//         return arr;
+
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter rows: ");
+//         int n = sc.nextInt();
+
+//         System.out.print("Enter columns: ");
+//         int m = sc.nextInt();
+
+//         int[][] arr = new int[n][m];
+//         System.out.println("Enter matrix elements:");
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < m; j++) {
+//                 arr[i][j] = sc.nextInt();
+//             }
+//         }
+
+//         int[][] result = matrixZeros(arr, n, m);
+
+//         System.out.println("Result matrix:");
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < m; j++) {
+//                 System.out.print(result[i][j] + " ");
+//             }
+//             System.out.println();
+//         }
+
+//         sc.close();
+//     }
+// }
+
+
+//set matrix zeros (optimal)
+public class Darray2 {
+
+    public static int[][] matrixZeros(int arr[][] ,  int n, int m){
+        int col0 = 1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(arr[i][j] == 0){
-                    row[i]=1;
-                    col[j]=1;
+                    arr[i][0]=0;
+                    if(j!=0){
+                        arr[0][j]=0;
+                    }else{
+                        col0=0;
+                    }
+                }
+            }
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++ ){
+                if(arr[i][0] == 0 || arr[0][j] == 0 ){
+                    arr[i][j] = 0;
 
                 }
             }
         }
+        if (arr[0][0] == 0) {
+            for (int j = 0; j < m; j++) {
+                arr[0][j] = 0;
+            }
+        }
 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(row[i]==1 || col[j]==1){
-                    arr[i][j]=0;
-                }
+        if (col0 == 0) {
+            for (int i = 0; i < n; i++) {
+                arr[i][0] = 0;
             }
         }
         return arr;
 
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
