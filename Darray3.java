8418a89a952fa9a095b1eb89a476827716186898
snehalgavhane33 <snehalgavhane33@gmail.com>
2026-07-31 22:10:43 +0530

    updated file

--- Darray3.java
+++ Darray3.java
@@ -100,36 +100,103 @@ import java.util.*;
 
 
 //majority element (n/3) better
+// public class Darray3 {
+
+//     public static List<Integer> majorityEle(int arr[] , int n){
+//         HashMap<Integer,Integer> map = new HashMap<>();
+//         List<Integer> ans = new ArrayList<>();
+//         int min = (n/3)+1;
+
+//         for(int i=0; i<n; i++){
+//             map.put(arr[i], map.getOrDefault(arr[i],0)+1 );
+//             if(map.get(arr[i])==min){
+//                 ans.add(arr[i]);
+//             }
+//             if(ans.size()==2) break;
+//         }
+//         Collections.sort(ans);
+//         return ans;
+//     }
+//      public static void main(String[] args) {
+//         Scanner sc= new Scanner(System.in);
+
+//         System.out.print("Enter size of array: ");
+//         int n = sc.nextInt();
+
+//         int[] arr = new int[n];
+
+//         System.out.println("Enter array elements:");
+//         for (int i = 0; i < n; i++) {
+//             arr[i] = sc.nextInt();
+//         }
+//          List<Integer> ans1 = majorityEle(arr, n);
+//          System.out.println("Majority Elements (> n/3 times): " + ans1);
+//     }
+// }
+
+//majority element (n/3) better
 public class Darray3 {
 
-    public static List<Integer> majorityEle(int arr[] , int n){
-        HashMap<Integer,Integer> map = new HashMap<>();
+     public static List<Integer> majorityEle(int[] nums, int n){
+        int cnt1=0,cnt2=0;
+        int ele1 = Integer.MAX_VALUE;
+        int ele2 = Integer.MIN_VALUE;
+
+        for(int i=0; i<n; i++){
+            if(cnt1==0 && nums[i] != ele2){
+                cnt1 = 1;
+                ele1 = nums[i];
+
+            }
+            else if(cnt2==00 && nums[i] != ele1){
+                cnt2 = 1;
+                ele2 = nums[i];
+            }
+            else if(nums[i] == ele1){
+                cnt1++;
+            }
+            else if(nums[i] == ele2){
+                cnt2++;
+            }
+            else{
+                cnt1--;
+                cnt2--;
+            }
+        }
+        cnt1 = 0;
+        cnt2 = 0;
+
+        for(int i=0; i<n; i++){
+            if(nums[i]==ele1){
+                cnt1++;
+            }
+            else if(nums[i]==ele2){
+                cnt2++;
+            }
+        }
         List<Integer> ans = new ArrayList<>();
         int min = (n/3)+1;
 
-        for(int i=0; i<n; i++){
-            map.put(arr[i], map.getOrDefault(arr[i],0)+1 );
-            if(map.get(arr[i])==min){
-                ans.add(arr[i]);
-            }
-            if(ans.size()==2) break;
-        }
-        Collections.sort(ans);
+        if(cnt1>= min)
+            ans.add(ele1);
+        if(cnt2>=min)
+            ans.add(ele2);
         return ans;
-    }
-     public static void main(String[] args) {
+    
+ }
+  public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
 
         System.out.print("Enter size of array: ");
         int n = sc.nextInt();
 
-        int[] arr = new int[n];
+        int[] nums = new int[n];
 
         System.out.println("Enter array elements:");
         for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+            nums[i] = sc.nextInt();
         }
-         List<Integer> ans1 = majorityEle(arr, n);
-         System.out.println("Majority Elements (> n/3 times): " + ans1);
+         List<Integer> ans = majorityEle(nums, n);
+         System.out.println("Majority Elements (> n/3 times): " + ans);
     }
 }
