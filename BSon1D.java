f1a9eef7365a8eaa10b0eba237e61396a9490c18
snehalgavhane33 <snehalgavhane33@gmail.com>
2026-08-08 20:41:35 +0530

    add Q2 lower bound

--- BSon1D.java
+++ BSon1D.java
@@ -46,26 +46,73 @@ import java.util.*;
 // }
 
 //Q1.Search X in sorted array(recursive code)
+// public class BSon1D {
+
+//     public static int bs(int arr[], int low , int high, int target){
+//         if(low> high) return -1;
+
+//          int mid = (low+high)/2;
+
+//          if(arr[mid]==target) return mid;
+//          else if(target>arr[mid]){
+//             return bs(arr, mid+1, high, target);
+//          }
+
+//          return bs(arr, low, mid - 1, target);
+//     }
+
+//     public static int binarySearch(int arr[], int n, int target){
+//         return bs(arr, 0, n-1, target);
+//     }
+
+//         public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter size of array: ");
+//         int n = sc.nextInt();
+
+//         int[] arr = new int[n];
+
+//         System.out.println("Enter array elements:");
+//         for (int i = 0; i < n; i++) {
+//             arr[i] = sc.nextInt();
+//         }
+
+//         System.out.print("Enter the target element: ");
+//         int target= sc.nextInt();
+
+//          int result = binarySearch(arr, n, target);
+//         if (result != -1) {
+//             System.out.println("Element found at index: " + result);
+//         } else {
+//             System.out.println("Element not found.");
+//         }
+//     }
+
+// }
+
+
+//Q2 lower bound
 public class BSon1D {
 
-    public static int bs(int arr[], int low , int high, int target){
-        if(low> high) return -1;
-
-         int mid = (low+high)/2;
-
-         if(arr[mid]==target) return mid;
-         else if(target>arr[mid]){
-            return bs(arr, mid+1, high, target);
-         }
-
-         return bs(arr, low, mid - 1, target);
+    public static int lowerBound(int arr[], int n, int target){
+        int low = 0;
+        int high = n-1;
+        int ans = n;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid]>=target){
+                ans=mid;
+                high = mid-1;
+            }
+            else{
+                low = mid +1;
+            }
+        }
+        return ans;
     }
 
-    public static int binarySearch(int arr[], int n, int target){
-        return bs(arr, 0, n-1, target);
-    }
-
-        public static void main(String[] args) {
+            public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter size of array: ");
@@ -81,12 +128,12 @@ public class BSon1D {
         System.out.print("Enter the target element: ");
         int target= sc.nextInt();
 
-         int result = binarySearch(arr, n, target);
-        if (result != -1) {
-            System.out.println("Element found at index: " + result);
+        int ans = lowerBound(arr, n, target);
+        if (ans == n) {
+            System.out.println("Lower Bound does not exist.");
         } else {
-            System.out.println("Element not found.");
+            System.out.println("Lower Bound Index: " + ans);
+            System.out.println("Lower Bound Element: " + arr[ans]);
         }
     }
-
 }
