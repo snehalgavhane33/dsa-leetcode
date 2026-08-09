ac426879272fcef58c9dab17a249651c4f7cbd0c
snehalgavhane33 <snehalgavhane33@gmail.com>
2026-08-09 20:34:25 +0530

    add floor and ceil Q

--- BSon1D.java
+++ BSon1D.java
@@ -185,45 +185,118 @@ import java.util.*;
 
 
 //Q4.Search insert position(same as lower bound)
-public class BSon1D {
+// public class BSon1D {
 
-    public static int insertPosition(int arr[], int n, int target){
-        int low = 0;
-        int high = n-1;
-        int ans = n;
-        while(low<=high){
-            int mid = (low+high)/2;
-            if(arr[mid]>=target){
-                ans = mid;
-                high = mid -1;
+//     public static int insertPosition(int arr[], int n, int target){
+//         int low = 0;
+//         int high = n-1;
+//         int ans = n;
+//         while(low<=high){
+//             int mid = (low+high)/2;
+//             if(arr[mid]>=target){
+//                 ans = mid;
+//                 high = mid -1;
 
-            }
-            else{
-                low = mid + 1;
-            }
-        }
-        return ans;
+//             }
+//             else{
+//                 low = mid + 1;
+//             }
+//         }
+//         return ans;
 
-    }
+//     }
 
     
-        public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+//         public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter size of array: ");
-        int n = sc.nextInt();
+//         System.out.print("Enter size of array: ");
+//         int n = sc.nextInt();
 
-        int[] arr = new int[n];
+//         int[] arr = new int[n];
 
-        System.out.println("Enter array elements:");
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
+//         System.out.println("Enter array elements:");
+//         for (int i = 0; i < n; i++) {
+//             arr[i] = sc.nextInt();
+//         }
 
-        System.out.print("Enter the target element: ");
-        int target= sc.nextInt();
+//         System.out.print("Enter the target element: ");
+//         int target= sc.nextInt();
 
-        int ans = insertPosition(arr, n, target);
-        System.out.println("Insert Position: " +insertPosition(arr, n, target));
-    }
-}
+//         int ans = insertPosition(arr, n, target);
+//         System.out.println("Insert Position: " +insertPosition(arr, n, target));
+//     }
+// }
+
+
+//Q5.Floor and Ceil in Sorted Array
+// Floor: Greatest element <= target
+// public class BSon1D {
+
+//     public static int floor(int arr[], int n , int target){
+//         int low = 0;
+//         int high = n-1;
+//         int ans = -1;
+//         while(low<=high){
+//             int mid = (low+high)/2;
+//             if(arr[mid]<=target){
+//                 ans =mid;
+//                 low = mid +1;
+//             }
+//             else{
+//                 high = mid - 1;
+//             }
+//         }
+//         return ans;
+//     }
+
+//      // Ceil: Smallest element >= target(lower bound)
+//     public static int ceil(int arr[], int n, int target) {
+//         int low = 0, high = n - 1;
+//         int ans = -1;
+//         while (low <= high) {
+//             int mid = (low + high) / 2;
+//             if (arr[mid] >= target) {
+//                 ans = mid;
+//                 high = mid - 1;
+//             } else {
+//                 low = mid + 1;
+//             }
+//         }
+//         return ans;
+//     }
+
+//      public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter size of array: ");
+//         int n = sc.nextInt();
+
+//         int[] arr = new int[n];
+
+//         System.out.println("Enter array elements:");
+//         for (int i = 0; i < n; i++) {
+//             arr[i] = sc.nextInt();
+//         }
+
+//         System.out.print("Enter the target element: ");
+//         int target= sc.nextInt();
+
+//          //Floor
+//         int floorIndex = floor(arr, n, target);
+//         if (floorIndex == -1)
+//             System.out.println("Floor does not exist.");
+//         else {
+//             System.out.println("Floor Index: " + floorIndex);
+//             System.out.println("Floor Element: " + arr[floorIndex]);
+//         }
+//         //Ceil
+//         int ceilIndex = ceil(arr, n, target);
+//         if (ceilIndex == -1)
+//             System.out.println("Ceil does not exist.");
+//         else {
+//             System.out.println("Ceil Index: " + ceilIndex);
+//             System.out.println("Ceil Element: " + arr[ceilIndex]);
+//         }
+//     }
+// }
