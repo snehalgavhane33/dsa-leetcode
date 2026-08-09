ac55a4266b6a1d6c5d3e38225e90587ba7978a98
snehalgavhane33 <snehalgavhane33@gmail.com>
2026-08-09 19:42:39 +0530

    add search and insert position Q

--- BSon1D.java
+++ BSon1D.java
@@ -139,24 +139,74 @@ import java.util.*;
 // }
 
 //Q3 upper bound
+// public class BSon1D {
+//     public static int upperBound(int arr[], int n, int target){
+//         int low = 0;
+//         int high = n-1;
+//         int ans = n;
+//         while(low<=high){
+//             int mid = (low+high)/2;
+//             if(arr[mid]>target){
+//                 ans=mid;
+//                 high = mid-1;
+//             }
+//             else{
+//                 low = mid +1;
+//             }
+//         }
+//         return ans;
+//     }
+
+//         public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter size of array: ");
+//         int n = sc.nextInt();
+
+//         int[] arr = new int[n];
+
+//         System.out.println("Enter array elements:");
+//         for (int i = 0; i < n; i++) {
+//             arr[i] = sc.nextInt();
+//         }
+
+//         System.out.print("Enter the target element: ");
+//         int target= sc.nextInt();
+
+//         int ans = upperBound(arr, n, target);
+//         if (ans == n) {
+//             System.out.println("Upper Bound does not exist.");
+//         } else {
+//             System.out.println("Upper Bound Index: " + ans);
+//             System.out.println("Upper Bound Element: " + arr[ans]);
+//         }
+//     }
+// }
+
+
+//Q4.Search insert position(same as lower bound)
 public class BSon1D {
-    public static int upperBound(int arr[], int n, int target){
+
+    public static int insertPosition(int arr[], int n, int target){
         int low = 0;
         int high = n-1;
         int ans = n;
         while(low<=high){
             int mid = (low+high)/2;
-            if(arr[mid]>target){
-                ans=mid;
-                high = mid-1;
+            if(arr[mid]>=target){
+                ans = mid;
+                high = mid -1;
+
             }
             else{
-                low = mid +1;
+                low = mid + 1;
             }
         }
         return ans;
+
     }
 
+    
         public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -173,12 +223,7 @@ public class BSon1D {
         System.out.print("Enter the target element: ");
         int target= sc.nextInt();
 
-        int ans = upperBound(arr, n, target);
-        if (ans == n) {
-            System.out.println("Upper Bound does not exist.");
-        } else {
-            System.out.println("Upper Bound Index: " + ans);
-            System.out.println("Upper Bound Element: " + arr[ans]);
-        }
+        int ans = insertPosition(arr, n, target);
+        System.out.println("Insert Position: " +insertPosition(arr, n, target));
     }
 }
