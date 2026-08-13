e4d53bd7ea6c405956472463f138fcee4a433eb0
snehalgavhane33 <snehalgavhane33@gmail.com>
2026-08-13 09:26:52 +0530

    add Q10.Find minimum in Rotated Sorted Array

--- BSon1D.java
+++ BSon1D.java
@@ -619,46 +619,105 @@ import java.util.*;
 //     }
 // }
 
-//Q8.Search in rotated sorted array-II
-public class BSon1D {
+//Q9.Search in rotated sorted array-II
+// public class BSon1D {
 
-    public static int rotateSearch(int arr[], int n, int target){
-        int low = 0;
-        int high = n-1;
-        while(low<=high){
-            int mid = (low+high)/2;
-            if(arr[mid]==target){
-                return mid;
-            }
-            if(arr[low]==arr[mid] && arr[mid]==arr[high]){
-                low = low + 1;
-                high = high - 1;
-                continue;
-            }
+//     public static int rotateSearch(int arr[], int n, int target){
+//         int low = 0;
+//         int high = n-1;
+//         while(low<=high){
+//             int mid = (low+high)/2;
+//             if(arr[mid]==target){
+//                 return mid;
+//             }
+//             if(arr[low]==arr[mid] && arr[mid]==arr[high]){
+//                 low = low + 1;
+//                 high = high - 1;
+//                 continue;
+//             }
             
-            if(arr[low]<=arr[mid]){
-                if(arr[low]<=target && target<arr[mid]){
-                    high=mid-1;
+//             if(arr[low]<=arr[mid]){
+//                 if(arr[low]<=target && target<arr[mid]){
+//                     high=mid-1;
 
-                }
-                else{
-                    low=mid+1;
-                }
-            }
-            else{
-                if(arr[mid]<target && target<=arr[high]){
-                    low=mid+1;
-                }
-                else{
-                    high = mid - 1;
+//                 }
+//                 else{
+//                     low=mid+1;
+//                 }
+//             }
+//             else{
+//                 if(arr[mid]<target && target<=arr[high]){
+//                     low=mid+1;
+//                 }
+//                 else{
+//                     high = mid - 1;
 
-                }
-            }
-        }
-        return -1;
-    }
+//                 }
+//             }
+//         }
+//         return -1;
+//     }
 
     
+//     public static void main(String[] args) {
+
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter size of array: ");
+//         int n = sc.nextInt();
+
+//         int arr[] = new int[n];
+
+//         System.out.println("Enter rotated sorted array:");
+
+//         for (int i = 0; i < n; i++) {
+//             arr[i] = sc.nextInt();
+//         }
+
+//         System.out.print("Enter target: ");
+//         int target = sc.nextInt();
+
+//         int ans = rotateSearch(arr, n, target);
+
+//         if (ans == -1) {
+//             System.out.println("Target not found");
+//         }
+//         else {
+//             System.out.println("Target found at index: " + ans);
+//         }
+
+//         sc.close();
+//     }
+// }
+
+
+//Q10.Find minimum in Rotated Sorted Array
+public class BSon1D{
+    public static int findMin(int arr[], int n){
+        int low = 0;
+        int high = n-1;
+        int ans= Integer.MAX_VALUE;
+        while(low<=high){
+            int mid = (low + high)/2;
+            if(arr[low]<=arr[high]){
+                ans = Math.min(ans,arr[low]);
+                break;}
+
+            if(arr[low]<=arr[mid]){
+                ans = Math.min(ans,arr[low]);
+                low = mid + 1;
+                
+            }
+            else{
+                ans = Math.min(ans,arr[mid]);
+                high = mid - 1;
+
+            }
+        }
+        return ans;
+
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -666,25 +725,16 @@ public class BSon1D {
         System.out.print("Enter size of array: ");
         int n = sc.nextInt();
 
-        int arr[] = new int[n];
+        int[] arr = new int[n];
 
         System.out.println("Enter rotated sorted array:");
-
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
 
-        System.out.print("Enter target: ");
-        int target = sc.nextInt();
+        int ans = findMin(arr, n);
 
-        int ans = rotateSearch(arr, n, target);
-
-        if (ans == -1) {
-            System.out.println("Target not found");
-        }
-        else {
-            System.out.println("Target found at index: " + ans);
-        }
+        System.out.println("Minimum element = " + ans);
 
         sc.close();
     }
