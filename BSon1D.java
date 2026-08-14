0e34caa822260e4622cd57e6950b499f6d5245fb
snehalgavhane33 <snehalgavhane33@gmail.com>
2026-08-14 11:45:59 +0530

    add Q13 find peak element

--- BSon1D.java
+++ BSon1D.java
@@ -804,32 +804,74 @@ import java.util.*;
 
 
  //Q12.Single element in a Sorted Array
- public class BSon1D {
+//  public class BSon1D {
  
-    public static int singleNonDuplicate(int arr[], int n){
-        if(n==1) return arr[0];
-        if(arr[0] != arr[1] ) return arr[0];
-        if(arr[n-1] != arr[n-2]) return arr[n-1];
-        int low = 1;
-        int high = n-2;
+//     public static int singleNonDuplicate(int arr[], int n){
+//         if(n==1) return arr[0];
+//         if(arr[0] != arr[1] ) return arr[0];
+//         if(arr[n-1] != arr[n-2]) return arr[n-1];
+//         int low = 1;
+//         int high = n-2;
+//         while(low<=high){
+//             int mid = (low+high)/2;
+//             if(arr[mid]!=arr[mid-1] && arr[mid]!=arr[mid+1]){
+//                 return arr[mid];
+//             }
+//             if(mid%2 ==1 && arr[mid]==arr[mid-1] || (mid%2==0 && arr[mid]==arr[mid+1])){
+//                 low = mid + 1;
+
+//             }
+//             else{
+//                 high = mid -1 ;
+//             }
+//         }
+
+//         return -1;
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter size of array: ");
+//         int n = sc.nextInt();
+
+//         int[] arr = new int[n];
+
+//         System.out.println("Enter array elements:");
+//         for (int i = 0; i < n; i++) {
+//             arr[i] = sc.nextInt();
+//         }
+
+//         System.out.print("Single element: " + singleNonDuplicate(arr, n));
+//     }
+
+//  }
+
+
+//Q13.Find peak element
+public class BSon1D{
+    public static int findPeak(int arr[], int n){
+        if(n==1) return 0;
+        if(arr[0]>arr[1]) return 0;
+        if(arr[n-1]>arr[n-2]) return n-1;
+        int low =0;
+        int high = n-1;
         while(low<=high){
             int mid = (low+high)/2;
-            if(arr[mid]!=arr[mid-1] && arr[mid]!=arr[mid+1]){
-                return arr[mid];
+            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
+                return mid;
             }
-            if(mid%2 ==1 && arr[mid]==arr[mid-1] || (mid%2==0 && arr[mid]==arr[mid+1])){
-                low = mid + 1;
-
+            else if(arr[mid]>arr[mid-1]){
+                low=mid+1;
             }
             else{
-                high = mid -1 ;
+                high=mid-1;
             }
         }
 
         return -1;
     }
-
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter size of array: ");
@@ -841,8 +883,6 @@ import java.util.*;
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-
-        System.out.print("Single element: " + singleNonDuplicate(arr, n));
+        System.out.print("Peak elementgit " + arr[findPeak(arr, n)]);
     }
-
- }
+}
