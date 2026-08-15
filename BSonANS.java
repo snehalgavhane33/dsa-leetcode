00b0a498cf128cf23db094a4fde62be37b186024
snehalgavhane33 <snehalgavhane33@gmail.com>
2026-08-15 16:04:10 +0530

    add Q3 function 3

--- BSonANS.java
+++ BSonANS.java
@@ -37,45 +37,96 @@ import java.util.*;
 
 
 //Q2.Find Nth root of a number
-public class BSonANS{
-    public static double multiply(double number,int n){
-        double ans = 1.0;
-        for(int i=1; i<=n; i++){
-            ans = ans * number;
-        }
-        return ans;
-    }
-    public static void getNthRoot(int n, int m){
-        double low = 1;
-        double high = m;
-        double eps = 1e-6;
-        while((high-low)>eps){
-            double mid = (low+high)/2.0;
-            if(multiply(mid,n)<m){
-                low = mid ;
-            }
-            else{
-                high = mid;
-            }
+// public class BSonANS{
+//     public static double multiply(double number,int n){
+//         double ans = 1.0;
+//         for(int i=1; i<=n; i++){
+//             ans = ans * number;
+//         }
+//         return ans;
+//     }
+//     public static void getNthRoot(int n, int m){
+//         double low = 1;
+//         double high = m;
+//         double eps = 1e-6;
+//         while((high-low)>eps){
+//             double mid = (low+high)/2.0;
+//             if(multiply(mid,n)<m){
+//                 low = mid ;
+//             }
+//             else{
+//                 high = mid;
+//             }
 
-        }
-        System.out.println(low + " " + high);
+//         }
+//         System.out.println(low + " " + high);
 
-        // Verification using Java's built-in Math.pow
-        System.out.println(Math.pow(m, 1.0 / n));
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+//         // Verification using Java's built-in Math.pow
+//         System.out.println(Math.pow(m, 1.0 / n));
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
         
-        System.out.println("enter n no.:");
-        int n = sc.nextInt();
-        System.out.println("enter m no.");
-        int m = sc.nextInt();
+//         System.out.println("enter n no.:");
+//         int n = sc.nextInt();
+//         System.out.println("enter m no.");
+//         int m = sc.nextInt();
         
-        getNthRoot(n, m);
+//         getNthRoot(n, m);
         
-        sc.close();
-    }
+//         sc.close();
+//     }
     
 
-}
+// }
+
+//koko eating bananas
+public class BSonANS {
+    public static int findMax(int[] v) {
+        int maxi = Integer.MIN_VALUE;
+        int n = v.length;
+        for (int i = 0; i < n; i++) {
+            maxi = Math.max(maxi, v[i]);
+        }
+        return maxi;
+    }
+    //Q3 function 2
+    public static int calculateTotalHours(int[] v, int hourly) {
+        int totalH = 0;
+        for (int i = 0; i < v.length; i++) {
+            totalH += Math.ceil((double) v[i] / hourly);
+        }
+        return totalH;
+    }
+    //Q3 function 3
+    public static int minEatingSpeed(int[] piles, int h) {
+        int low = 1;
+        int high = findMax(piles);
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int totalH = calculateTotalHours(piles, mid);
+            if (totalH <= h) {
+                high = mid - 1;
+            } 
+            else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of piles: ");
+        int n = sc.nextInt();
+        int[] piles = new int[n];
+        System.out.println("Enter bananas in each pile:");
+        for (int i = 0; i < n; i++) {
+            piles[i] = sc.nextInt();
+        }
+        System.out.print("Enter maximum hours: ");
+        int h = sc.nextInt();
+        System.out.println("Minimum eating speed = " + minEatingSpeed(piles, h));
+        }
+    }
+
